ddf57f203fc7b17115f1fc6d77450ddc7ded8f3b
Nate McMaster <nate.mcmaster@gmail.com>
2014-01-31 12:59:19 -0700

    First test of log

--- test/js/model/LogTest.js
+++ test/js/model/LogTest.js
@@ -1,3 +1,31 @@
-test( "hello test", function() {
-  ok( 1 == "1", "Passed!" );
+var MockProxy=require('../MockProxy.js');
+var Log=require('../../../gameplay/js/model/Log.js');
+var assert=require('assert');
+
+suite('LogTests',function(){
+	var log,testData;
+
+	setup(function(){
+		testData= {
+			"lines": [{
+				"source": "Sam",
+				"message": "Sam built a road"
+			},{
+				"source": "Brooke",
+				"message": "Brooke built a settlement"
+			}]
+		};
+		var mockProxy=new MockProxy();
+		log= new Log(mockProxy,testData);
+	});
+
+	describe('#mostRecentEntry()',function(){
+		test('should return "Brooke"',function(){
+			assert.equal(log.mostRecentEntry(),testData.lines[1]);
+		})
+		
+	})
+
+
+
 });
