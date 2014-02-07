d51ca2334adabeaa6c870d9b6e67ca5d80014c48
Nate McMaster <nate.mcmaster@gmail.com>
2014-02-07 01:01:56 -0700

    Add tests for rob player

--- src/js/model/board/map/index.js
+++ src/js/model/board/map/index.js
@@ -1,10 +1,9 @@
-
 module.exports = {
-  Edge: require('./Edge.js'),
-  Hex: require('./Hex'),
-  Map: require('./Map'),
-  Port: require('./Port'),
-  NumberTiles: require('./NumberTiles'),
-  Vertex: require('./Vertex')
+	hexgrid: require('./hexgrid'),
+	Edge: require('./Edge.js'),
+	Hex: require('./Hex'),
+	Map: require('./Map'),
+	Port: require('./Port'),
+	NumberTiles: require('./NumberTiles'),
+	Vertex: require('./Vertex')
 }
-
--- test/js/model/board/GameBoardTest.js
+++ test/js/model/board/GameBoardTest.js
@@ -1,28 +1,53 @@
+var fixture = require('./example.json'),
+	expect = require('chai').expect,
+	GameBoard = require('./impl').GameBoard,
+	HexLocation = require('./impl').map.hexgrid.HexLocation,
+	MockProxy = require('../../MockProxy'),
+	expect = require('chai').expect;
 
-var fixture = require('./example.json')
-  , expect = require('chai').expect
-  , GameBoard = require('./impl').GameBoard;
+suite('GameBoard', function() {
+		suite('#constructor', function() {
+			var gb;
+			setup(function() {
+				gb = new GameBoard(null, fixture)
+			})
+			test('should understand game over', function() {
+				expect(gb.isGameOver()).to.be.false;
+			})
+			test('should poppulate largestArmyOwner', function() {
+				expect(gb.bigArmyOwner()).to.equal(2)
+			})
+			test('should poppulate longestRoadOwner', function() {
+				expect(gb.longRoadOwner()).to.equal(0)
+			})
+			test('should populate other stuff', function() {
+				expect(gb.players).to.be.ok;
+				expect(gb.bank).to.be.ok;
+				expect(gb.deck).to.be.ok;
+			})
+		});
 
-suite('GameBoard', function () {
-	suite('#constructor', function () {
-		var gb
-		setup(function () {
-			gb = new GameBoard(null, fixture)
-		})
-		test('should understand game over', function () {
-			expect(gb.isGameOver()).to.be.false;
-		})
-		test('should poppulate largestArmyOwner', function () {
-			expect(gb.bigArmyOwner()).to.equal(2)
-		})
-		test('should poppulate longestRoadOwner', function () {
-			expect(gb.longRoadOwner()).to.equal(0)
-		})
-		test('should populate other stuff', function () {
-			expect(gb.players).to.be.ok;
-			expect(gb.bank).to.be.ok;
-			expect(gb.deck).to.be.ok;
-		})
-	})
-})
+		suite('integration with proxy', function() {
+			var mockProxy, gb;
+			setup(function() {
+				mockProxy = new MockProxy;
+				gb = new GameBoard(mockProxy, fixture);
+			});
+			test('#robPlayer()', function() {
+				var expected = {
+					'type': 'robPlayer',
+					'playerIndex': 1,
+					'victimIndex': 2,
+					'robberSpot': {
+						'x': -3,
+						'y': 0
+					}
+				};
 
+				gb.robPlayer(expected.playerIndex, expected.victimIndex, new HexLocation(-3, 0));
+
+				expect(mockProxy.lastCommand.getData()).to.deep.equal(expected);
+			});
+
+		});
+	});
