44ce9a022436a67d9db73513c7ef7f284eddbb78
Alan Dayton <alan.dayton.1@gmail.com>
2014-02-07 12:59:56 -0700

    Update BankTest.js to avoid code duplication

--- test/js/model/board/BankTest.js
+++ test/js/model/board/BankTest.js
@@ -5,138 +5,51 @@ var assert = require('chai').assert;
 
 suite('BankTests', function() {
 
-	suite('bank has no resources', function() {
+	var testCases = [
+		{
+			'description':'bank has no resources',
+			'input':{'brick':0,'wood':0,'sheep':0,'wheat':0,'ore':0},
+			'output':{'brick':false,'wood':false,'sheep':false,'wheat':false,'ore':false}
+		},
+		{
+			'description':'bank has one of each resource',
+			'input':{'brick':1,'wood':1,'sheep':1,'wheat':1,'ore':1},
+			'output':{'brick':true,'wood':true,'sheep':true,'wheat':true,'ore':true}
+		},
+		{
+			'description':'bank has some resources but not others',
+			'input':{'brick':0,'wood':5,'sheep':0,'wheat':0,'ore':2},
+			'output':{'brick':false,'wood':true,'sheep':false,'wheat':false,'ore':true}
+		},
+		{
+			'description':'bank has multiple of each resource',
+			'input':{'brick':3,'wood':2,'sheep':4,'wheat':3,'ore':5},
+			'output':{'brick':true,'wood':true,'sheep':true,'wheat':true,'ore':true}
+		},
+		{
+			'description':'bank is full',
+			'input':{'brick':19,'wood':19,'sheep':19,'wheat':19,'ore':19},
+			'output':{'brick':true,'wood':true,'sheep':true,'wheat':true,'ore':true}
+		}
+	];
 
-		var bank;
+	testCases.forEach(function(testCase) {
 
-		setup(function() {
+		suite(testCase.description, function() {
+			var bank;
 
-			var testData = {
-				"brick":0,
-				"wood":0,
-				"sheep":0,
-				"wheat":0,
-				"ore":0
-			};
+			setup(function() {
+				var mockProxy = new MockProxy();
+				bank = new Bank(mockProxy, testCase.input);
+			});
 
-			var mockProxy = new MockProxy();
-			bank = new Bank(mockProxy, testData);
-		});
-
-		test('#canWithdraw()', function() {
-			assert.isFalse(bank.canWithdraw(ResourceType.BRICK));
-			assert.isFalse(bank.canWithdraw(ResourceType.ORE));
-			assert.isFalse(bank.canWithdraw(ResourceType.SHEEP));
-			assert.isFalse(bank.canWithdraw(ResourceType.WHEAT));
-			assert.isFalse(bank.canWithdraw(ResourceType.WOOD));
-		});
-	});
-
-	suite('bank has one of each resource', function() {
-
-		var bank;
-
-		setup(function() {
-
-			var testData = {
-				"brick":1,
-				"wood":1,
-				"sheep":1,
-				"wheat":1,
-				"ore":1
-			};
-
-			var mockProxy = new MockProxy();
-			bank = new Bank(mockProxy, testData);
-		});
-
-		test('#canWithdraw()', function() {
-			assert.isTrue(bank.canWithdraw(ResourceType.BRICK));
-			assert.isTrue(bank.canWithdraw(ResourceType.WOOD));
-			assert.isTrue(bank.canWithdraw(ResourceType.SHEEP));
-			assert.isTrue(bank.canWithdraw(ResourceType.WHEAT));
-			assert.isTrue(bank.canWithdraw(ResourceType.ORE));
-		});
-	});
-
-	suite('bank has some resources but not others', function() {
-
-		var bank;
-
-		setup(function() {
-
-			var testData = {
-				"brick":0,
-				"wood":5,
-				"sheep":0,
-				"wheat":0,
-				"ore":2
-			};
-
-			var mockProxy = new MockProxy();
-			bank = new Bank(mockProxy, testData);
-		});
-
-		test('#canWithdraw()', function() {
-			assert.isFalse(bank.canWithdraw(ResourceType.BRICK));
-			assert.isTrue(bank.canWithdraw(ResourceType.WOOD));
-			assert.isFalse(bank.canWithdraw(ResourceType.SHEEP));
-			assert.isFalse(bank.canWithdraw(ResourceType.WHEAT));
-			assert.isTrue(bank.canWithdraw(ResourceType.ORE));
-		});
-	});
-
-	suite('bank has multiple of each resource', function() {
-
-		var bank;
-
-		setup(function() {
-
-			var testData = {
-				"brick":3,
-				"wood":2,
-				"sheep":4,
-				"wheat":2,
-				"ore":5
-			};
-
-			var mockProxy = new MockProxy();
-			bank = new Bank(mockProxy, testData);
-		});
-
-		test('#canWithdraw()', function() {
-			assert.isTrue(bank.canWithdraw(ResourceType.BRICK));
-			assert.isTrue(bank.canWithdraw(ResourceType.WOOD));
-			assert.isTrue(bank.canWithdraw(ResourceType.SHEEP));
-			assert.isTrue(bank.canWithdraw(ResourceType.WHEAT));
-			assert.isTrue(bank.canWithdraw(ResourceType.ORE));
-		});
-	});
-
-	suite('bank is full', function() {
-
-		var bank;
-
-		setup(function() {
-
-			var testData = {
-				"brick":19,
-				"wood":19,
-				"sheep":19,
-				"wheat":19,
-				"ore":19
-			};
-
-			var mockProxy = new MockProxy();
-			bank = new Bank(mockProxy, testData);
-		});
-
-		test('#canWithdraw()', function() {
-			assert.isTrue(bank.canWithdraw(ResourceType.BRICK));
-			assert.isTrue(bank.canWithdraw(ResourceType.WOOD));
-			assert.isTrue(bank.canWithdraw(ResourceType.SHEEP));
-			assert.isTrue(bank.canWithdraw(ResourceType.WHEAT));
-			assert.isTrue(bank.canWithdraw(ResourceType.ORE));
+			test('#canWithdraw()', function() {
+				assert.equal(testCase.output[ResourceType.BRICK], bank.canWithdraw(ResourceType.BRICK));
+				assert.equal(testCase.output[ResourceType.WOOD], bank.canWithdraw(ResourceType.WOOD));
+				assert.equal(testCase.output[ResourceType.SHEEP], bank.canWithdraw(ResourceType.SHEEP));
+				assert.equal(testCase.output[ResourceType.WHEAT], bank.canWithdraw(ResourceType.WHEAT));
+				assert.equal(testCase.output[ResourceType.ORE], bank.canWithdraw(ResourceType.ORE));
+			})
 		});
 	});
 });
