4044da37d8215d212b897a106fa7d667a31078a1
Jared Forsyth <jared@jaredforsyth.com>
2014-02-12 12:28:45 -0700

    adding index

---
+++ src/js/controllers/index.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+	BasicController: require('./BasicController'),
+	CardController: require('./CardController'),
+	CommController: require('./CommController'),
+	DiscardController: require('./DiscardController'),
+	DomesticController: require('./DomesticController'),
+	MapController: require('./MapController'),
+	MaritimeController: require('./MaritimeController'),
+	PointsController: require('./PointsController'),
+	ResourcesController: require('./ResourcesController'),
+	RollController: require('./RollController'),
+	SetupRoundConnections: require('./SetupRoundConnections'),
+	SetupRoundController: require('./SetupRoundController'),
+	TrackerController: require('./TrackerController')
+}
+
