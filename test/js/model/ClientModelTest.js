960005c843661e7d336f1c34ed80576da1805125
Alan Dayton <alan.dayton.1@gmail.com>
2014-02-26 12:11:41 -0700

    Adding ClientModelTests

---
+++ test/js/model/ClientModelTest.js
@@ -0,0 +1,9 @@
+var MockProxy = require('../MockProxy.js');
+var ClientModel = require('../../../src/js/model/clientmodel.js');
+var ClientModelTestCases = require('./ClientModelTestCases.json');
+var assert = require('chai').assert;
+
+suite('ClientModelTests', function() {
+
+	
+});
