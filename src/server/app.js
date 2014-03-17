00a683155494f288bdffd17c552c782ca834d0c3
Spencer Gardner <spence450@gmail.com>
2014-03-17 10:47:07 -0600

    started front controller middleware, added package.json for future dependencies

--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,11 @@
 /**
  * Module dependencies.
  */
-
-var express = require('express');
-var http = require('http');
-var path = require('path');
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    authware = require('./middleware/auth')
+    ;
 
 var app = express();
 
@@ -15,10 +16,14 @@ app.set('port', process.env.PORT || 8081);
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use(authware);
 //app.use(express.json());
+app.use(express.bodyParser());
+app.use(express.cookieParser());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
+
 
 app.use('/',express.static(path.resolve(buildRoot, './gameplay')));
 app.use('/docs/',express.static(path.resolve(buildRoot, './docs/')));
@@ -28,12 +33,14 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// controller instantiation
 var controllers = require('./controllers');
 var model={};
 for(var c in controllers){
 	var d = new controllers[c](app,model);
 }
 
+// start server
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Server listening on port ' + app.get('port'));
 });
