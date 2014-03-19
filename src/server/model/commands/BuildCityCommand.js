c0c7dd52d54cfe428f49d63cad5eaab3bd95c24e
Jared Forsyth <jared@jaredforsyth.com>
2014-03-19 12:35:49 -0600

    adding abstract move command

---
+++ src/server/model/commands/BuildCityCommand.js
@@ -0,0 +1,18 @@
+
+var AbstractMoveCommand = require('./AbstractMoveCommand')
+  , util = require('util')
+
+module.exports = BuildCityCommand;
+util.inherits(BuildCityCommand, AbstractMoveCommand)
+
+function BuildCityCommand(gameId, playerIndex, cityLocation, isFree) {
+  AbstractMoveCommand.call(this, gameId);
+  this.playerIndex = playerIndex;
+  this.cityLocation = cityLocation;
+  this.isFree = isFree;
+}
+
+BuildCityCommand.prototype.executeOnGame = function (game) {
+  // do something
+}
+
