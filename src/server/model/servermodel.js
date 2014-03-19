e09aec4a7785d64ff6ed3c542392d96360cc86e3
Alan Dayton <alan.dayton.1@gmail.com>
2014-03-18 22:24:52 -0600

    Stubbed out functions on server model

--- src/server/model/servermodel.js
+++ src/server/model/servermodel.js
@@ -1,11 +1,3 @@
-var GameBoard = require("./board/GameBoard");
-var Chat = require("./Chat");
-var Log = require("./Log");
-var Proxy = require("./Proxy");
-
-var Definitions = require('byu-catan').definitions;
-var ResourceTypes = Definitions.ResourceTypes;
-
 module.exports = ServerModel;
 
 /**
@@ -20,73 +12,85 @@ catan.models = catan.models || {};
 
 catan.models.ServerModel = ServerModel
 /** 
-* This the top-level server model class that contains the local player, map contents, etc.
+* This the top-level server model class
 
 * @class ServerModel
 * @constructor
 * @param {integer} playerID The id of the local player, extracted from the cookie
 */
-function ServerModel(playerID){
-  this.playerID = playerID;
-  this.observers=[];
-  this.revision = -1;
-}      
-
-/**
-  * This is called to fetch the game state from the server the very first time.
-  * It should: 1) fetch the game state JSON from the server, 2) update the client model with the
-  * returned data, 3) notify all client model listeners that the model has changed, and 4) invoke
-  * the success callback function with the object received from the server.
-  * 
-  * @method initFromServer
-  * @param {function} success - A callback function that is called after the game state has been fetched from the server and the client model updated. This function is passed a single parameter which is the game state object received from the server.
-  */
-ClientModel.prototype.initFromServer = function(success){
-  this.proxy = new Proxy(this.populateModels.bind(this));
-  this.proxy.getModel(function(err, data){
-    try{
-      this.populateModels(data); 
-    }
-    catch(e){
-      console.error(e);
-      return;
-    }
-    
-    success();
-
-  }.bind(this));
-
-  this.proxy.startPolling();
-}
-
-/**
- * Updates the models if the revision number has changed.
- * @param  {object} data Raw data from server
- * @return {void}      
- */
-ClientModel.prototype.populateModels = function (data) {
+function ServerModel(data, $Log, $Chat, $GameBoard){
   if(!data || !data.log || !data.chat || 'undefined' === typeof data.revision)
     throw new TypeError('invalid data model');
 
-  if(this.revision === data.revision){
-      return;
-  }
-  this.revision = data.revision;
-  this.log = new Log(this.proxy, data.log);
-  this.chat = new Chat(this.proxy, data.chat);
-  try{
-    this.gameboard = new GameBoard(this.proxy, data);
-  }
-  catch(e){
-    throw new TypeError('invalid gameboard model');
-  }
-  for (var x in data.players) {
-    if (data.players[x].playerID == this.playerID) {
-      this.playerIndex = +x;
-      break;
-    }
-  }
-  this.notifyObservers();
-}
+  this.log = $Log(data.log);
+  this.chat = $Chat(data.chat);
+  this.gameboard = $GameBoard(data);
+};
 
+ServerModel.prototype.sendChat = function(playerIndex, message) {
 
+};
+
+ServerModel.prototype.rollNumber = function(playerIndex, number) {
+
+};
+
+ServerModel.prototype.robPlayer = function(playerIndex, victimIndex, location) {
+
+};
+
+ServerModel.prototype.finishTurn = function(playerIndex) {
+
+};
+
+ServerModel.prototype.buyDevCard = function(playerIndex) {
+
+};
+
+ServerModel.prototype.playYearOfPlenty = function(playerIndex, resource1, resource2) {
+
+};
+
+ServerModel.prototype.playRoadBuilding = function(playerIndex, spot1, spot2) {
+
+};
+
+ServerModel.prototype.playSoldier = function(playerIndex, victimIndex, location) {
+
+};
+
+ServerModel.prototype.playMonopoly = function(playerIndex, resource) {
+
+};
+
+ServerModel.prototype.playMonument = function(playerIndex) {
+
+};
+
+ServerModel.prototype.buildRoad = function(playerIndex, roadLocation, free) {
+
+};
+
+ServerModel.prototype.buildSettlement = function(playerIndex, vertexLocation, free) {
+
+};
+
+ServerModel.prototype.buildCity = function(playerIndex, vertexLocation, free) {
+
+};
+
+ServerModel.prototype.offerTrade = function(playerIndex, offer, receiver) {
+
+};
+
+ServerModel.prototype.acceptTrade = function(playerIndex, willAccept) {
+
+};
+
+ServerModel.prototype.maritimeTrade = function(playerIndex, ratio, inputResource, outputResource) {
+
+};
+
+ServerModel.prototype.discardCards = function(playerIndex, cardsToDiscard) {
+
+};
