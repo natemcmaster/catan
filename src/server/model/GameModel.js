2d385a377b613006e59cec4019509bf773af227a
Chris Brown <smartcab@gmail.com>
2014-03-20 19:50:22 -0600

    Fixed typo in game model

--- src/server/model/GameModel.js
+++ src/server/model/GameModel.js
@@ -62,73 +62,73 @@ if(!data){
   this.gameboard = $GameBoard(data);
 };
 
-Game.prototype.sendChat = function(playerIndex, message) {
+GameModel.prototype.sendChat = function(playerIndex, message) {
+	this.sendChat(playerIndex,message);
+};
+
+GameModel.prototype.rollNumber = function(playerIndex, number) {
 
 };
 
-Game.prototype.rollNumber = function(playerIndex, number) {
+GameModel.prototype.robPlayer = function(playerIndex, victimIndex, location) {
 
 };
 
-Game.prototype.robPlayer = function(playerIndex, victimIndex, location) {
+GameModel.prototype.finishTurn = function(playerIndex) {
 
 };
 
-Game.prototype.finishTurn = function(playerIndex) {
-
-};
-
-Game.prototype.buyDevCard = function(playerIndex) {
+GameModel.prototype.buyDevCard = function(playerIndex) {
 	this.players[playerIndex].buyDevCard(this.deck.drawRandomCard());
 };
 
-Game.prototype.playYearOfPlenty = function(playerIndex, resource1, resource2) {
+GameModel.prototype.playYearOfPlenty = function(playerIndex, resource1, resource2) {
 	this.bank.withdraw(resource1);
 	this.bank.withdraw(resource2);
 
 	players[playerIndex].playYearOfPlenty(resource1, resource2);
 };
 
-Game.prototype.playRoadBuilding = function(playerIndex, spot1, spot2) {
+GameModel.prototype.playRoadBuilding = function(playerIndex, spot1, spot2) {
 
 };
 
-Game.prototype.playSoldier = function(playerIndex, victimIndex, location) {
+GameModel.prototype.playSoldier = function(playerIndex, victimIndex, location) {
 
 };
 
-Game.prototype.playMonopoly = function(playerIndex, resource) {
+GameModel.prototype.playMonopoly = function(playerIndex, resource) {
 
 };
 
-Game.prototype.playMonument = function(playerIndex) {
+GameModel.prototype.playMonument = function(playerIndex) {
 
 };
 
-Game.prototype.buildRoad = function(playerIndex, roadLocation, free) {
+GameModel.prototype.buildRoad = function(playerIndex, roadLocation, free) {
 
 };
 
-Game.prototype.buildSettlement = function(playerIndex, vertexLocation, free) {
+GameModel.prototype.buildSettlement = function(playerIndex, vertexLocation, free) {
 
 };
 
-Game.prototype.buildCity = function(playerIndex, vertexLocation, free) {
+GameModel.prototype.buildCity = function(playerIndex, vertexLocation, free) {
 
 };
 
-Game.prototype.offerTrade = function(playerIndex, offer, receiver) {
+GameModel.prototype.offerTrade = function(playerIndex, offer, receiver) {
 
 };
 
-Game.prototype.acceptTrade = function(playerIndex, willAccept) {
+GameModel.prototype.acceptTrade = function(playerIndex, willAccept) {
 
 };
 
-Game.prototype.maritimeTrade = function(playerIndex, ratio, inputResource, outputResource) {
+GameModel.prototype.maritimeTrade = function(playerIndex, ratio, inputResource, outputResource) {
 
 };
 
-Game.prototype.discardCards = function(playerIndex, cardsToDiscard) {
+GameModel.prototype.discardCards = function(playerIndex, cardsToDiscard) {
 
 };
