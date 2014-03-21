ab6678b0f27f273fe389a0bd2658a08611b0f9da
Nate McMaster <nate.mcmaster@gmail.com>
2014-03-21 17:56:54 -0600

    Added documentation for injector

---
+++ injector/api.js
@@ -0,0 +1,11 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Injector",
+        "Injector.Dependency",
+        "InjectorError"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
