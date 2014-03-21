cc442813c609d6a71cb93e110b7474413b1e4160
Jared Forsyth <jared@jaredforsyth.com>
2014-03-20 18:55:12 -0600

    renaming all the things

--- src/server/model/Game.js
+++ src/server/model/Game.js
@@ -1,17 +1,17 @@
 var BaseModel = require('./BaseModel');
 var util = require('util');
 
-module.exports = ServerModel;
-util.inherits(ServerModel, BaseModel);
+module.exports = Game;
+util.inherits(Game, BaseModel);
 
 /** 
 * This is the server model class
 
-* @class ServerModel
+* @class Game
 * @constructor
 * @param {data} playerID The id of the local player, extracted from the cookie
 */
-function ServerModel(data, $Log, $Chat, $GameBoard){
+function Game(data, $Log, $Chat, $GameBoard){
   if(!data)
     throw new TypeError('invalid data model');
 
@@ -20,70 +20,70 @@ function ServerModel(data, $Log, $Chat, $GameBoard){
   this.gameboard = $GameBoard(data);
 };
 
-ServerModel.prototype.sendChat = function(playerIndex, message) {
+Game.prototype.sendChat = function(playerIndex, message) {
 
 };
 
-ServerModel.prototype.rollNumber = function(playerIndex, number) {
+Game.prototype.rollNumber = function(playerIndex, number) {
 
 };
 
-ServerModel.prototype.robPlayer = function(playerIndex, victimIndex, location) {
+Game.prototype.robPlayer = function(playerIndex, victimIndex, location) {
 
 };
 
-ServerModel.prototype.finishTurn = function(playerIndex) {
+Game.prototype.finishTurn = function(playerIndex) {
 
 };
 
-ServerModel.prototype.buyDevCard = function(playerIndex) {
+Game.prototype.buyDevCard = function(playerIndex) {
 
 };
 
-ServerModel.prototype.playYearOfPlenty = function(playerIndex, resource1, resource2) {
+Game.prototype.playYearOfPlenty = function(playerIndex, resource1, resource2) {
 
 };
 
-ServerModel.prototype.playRoadBuilding = function(playerIndex, spot1, spot2) {
+Game.prototype.playRoadBuilding = function(playerIndex, spot1, spot2) {
 
 };
 
-ServerModel.prototype.playSoldier = function(playerIndex, victimIndex, location) {
+Game.prototype.playSoldier = function(playerIndex, victimIndex, location) {
 
 };
 
-ServerModel.prototype.playMonopoly = function(playerIndex, resource) {
+Game.prototype.playMonopoly = function(playerIndex, resource) {
 
 };
 
-ServerModel.prototype.playMonument = function(playerIndex) {
+Game.prototype.playMonument = function(playerIndex) {
 
 };
 
-ServerModel.prototype.buildRoad = function(playerIndex, roadLocation, free) {
+Game.prototype.buildRoad = function(playerIndex, roadLocation, free) {
 
 };
 
-ServerModel.prototype.buildSettlement = function(playerIndex, vertexLocation, free) {
+Game.prototype.buildSettlement = function(playerIndex, vertexLocation, free) {
 
 };
 
-ServerModel.prototype.buildCity = function(playerIndex, vertexLocation, free) {
+Game.prototype.buildCity = function(playerIndex, vertexLocation, free) {
 
 };
 
-ServerModel.prototype.offerTrade = function(playerIndex, offer, receiver) {
+Game.prototype.offerTrade = function(playerIndex, offer, receiver) {
 
 };
 
-ServerModel.prototype.acceptTrade = function(playerIndex, willAccept) {
+Game.prototype.acceptTrade = function(playerIndex, willAccept) {
 
 };
 
-ServerModel.prototype.maritimeTrade = function(playerIndex, ratio, inputResource, outputResource) {
+Game.prototype.maritimeTrade = function(playerIndex, ratio, inputResource, outputResource) {
 
 };
 
-ServerModel.prototype.discardCards = function(playerIndex, cardsToDiscard) {
+Game.prototype.discardCards = function(playerIndex, cardsToDiscard) {
 
 };
