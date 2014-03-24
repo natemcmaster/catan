e9c75f187b6a0631d22640023f85dfc0ea01fd01
Spencer Gardner <spence450@gmail.com>
2014-03-23 20:38:43 -0600

    changes

--- src/server/middleware/game.js
+++ src/server/middleware/game.js
@@ -1,16 +1,22 @@
-// Game cookie is set upon user's joining a game
 // attach game ID to req object
 // attached userIndex to reqObject
   // user index is calculated by going into the correct game, looking up the player in the users array of the game that has the global playerID, and then based on position [0-3], setting that
 var _ = require('lodash');
 
 module.exports = function(req, res, next){
-  var catanGameCookie = req.cookies['catan.game'];
+  var catanGameNumber = req.cookies['catan.game'];
 
-  if(catanGameCookie) {
-    req.gameID = parsedCatanGameCookie;
-    //req.userIndex = TODO: calculate game-specific userIndex based on global playerId in cookie
+  if(catanGameNumber) {
+    req.gameId = catanGameNumber;
 
+    var game = _.find(req.gameRoom.listGames(), function(game){
+      return game.id == req.gameId;
+    });
+
+    // sets corresponding local playerIndex using global req.playerID set in the 'auth.js' middleware
+    req.userIndex = game.indexOf(req.playerID);
+
+    next();
   } else {
     res.send(400);
   }
