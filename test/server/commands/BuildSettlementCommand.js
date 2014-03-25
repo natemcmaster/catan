d1a8f26b99d00cdf0ff5013ccc550e23142ccc0c
Spencer Gardner <spence450@gmail.com>
2014-03-24 21:51:17 -0600

    further buildsettlementcommand tests

--- test/server/commands/BuildSettlementCommand.js
+++ test/server/commands/BuildSettlementCommand.js
@@ -8,7 +8,7 @@ var expect = require('chai').expect
 describe('buildSettlementCommand', function () {
   h.loggedInAs('Sam', 'sam', function () {
     h.inGame(1, function () {
-      it('should work', function (done) {
+      it('should place settlement on right location', function (done) {
         var app = this.app
           , game = app.gameRoom.getGameModel(1);
         this.agent.post('/moves/buildSettlement')
@@ -34,6 +34,44 @@ describe('buildSettlementCommand', function () {
             done();
           });
       });
+
+      it('should make bank receive payment for settlement', function (done) {
+        var app = this.app
+            , game = app.gameRoom.getGameModel(1)
+            , sam = game.players[0];
+        expect(sam.resources['brick']).to.equal(14);
+        expect(sam.resources['wood']).to.equal(13);
+        expect(sam.resources['sheep']).to.equal(15);
+        expect(sam.resources['wheat']).to.equal(10);
+
+        this.agent.post('/moves/buildSettlement')
+          .send({
+            "playerIndex": 0,
+            "vertexLocation": {
+                "x": -2,
+                "y": 1,
+                "direction": "E"
+            },
+            "free": false
+          })
+          .expect(200)
+          .end(function (err, res) {
+            if (err) {
+              console.error(res.text);
+              return done(err);
+            }
+
+            expect(sam.resources['brick']).to.equal(13);
+            expect(sam.resources['wood']).to.equal(12);
+            expect(sam.resources['sheep']).to.equal(14);
+            expect(sam.resources['wheat']).to.equal(9);
+
+            done();
+          });
+      });
+
+
+
 
 
 
