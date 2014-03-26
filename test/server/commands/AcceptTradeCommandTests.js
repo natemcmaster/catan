02c0122013af423e2869af42cb1211a7ebe57886
Alan Dayton <alan.dayton.1@gmail.com>
2014-03-25 19:01:38 -0600

    Add test for accept trade command

--- test/server/commands/AcceptTradeCommandTests.js
+++ test/server/commands/AcceptTradeCommandTests.js
@@ -42,6 +42,39 @@ describe('Accept Trade Command', function () {
             done();
           });
       });
+
+      it('should reject the trade', function (done) {
+        var app = this.app
+          , game = app.gameRoom.getGameModel(1)
+          , offererIndex = 0
+          , offererCurrentHand = {brick:14, wood:13, sheep:15, wheat:10, ore:8}
+          , receiverIndex = 3
+          , receiverCurrentHand = {brick:0, wood:1, sheep:1, wheat:0, ore:2}
+          , offer = {brick:-2, wood:1, sheep:-1, wheat:0, ore:0}
+          , tradeOffer = {'sender':offererIndex, 'receiver':receiverIndex, 'offer':offer}
+          , willAccept = false
+
+        game.data.tradeOffer = tradeOffer;
+        game.players[offererIndex].setResources(offererCurrentHand);
+        game.players[receiverIndex].setResources(receiverCurrentHand);
+
+        this.agent.post('/moves/acceptTrade')
+          .send({'playerIndex':receiverIndex, 'willAccept':willAccept})
+          .expect(200)
+          .end(function (err, res) {
+            if (err) {
+              console.error(res.text);
+              return done(err);
+            }
+
+            var game = app.gameRoom.getGameModel(1);
+            expect(game.players[offererIndex].getResources()).to.deep.equal(offererCurrentHand);
+            expect(game.players[receiverIndex].getResources()).to.deep.equal(receiverCurrentHand);
+            expect(game.data.tradeOffer).to.be.undefined;
+
+            done();
+          });
+      });
     });
   });
 });
