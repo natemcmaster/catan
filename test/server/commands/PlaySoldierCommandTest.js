ee1daf139ad3e3dd830c7208427ef761894b5708
Alan Dayton <alan.dayton.1@gmail.com>
2014-03-28 12:58:52 -0600

    Add tests for largest army

--- test/server/commands/PlaySoldierCommandTest.js
+++ test/server/commands/PlaySoldierCommandTest.js
@@ -6,7 +6,7 @@ var expect = require('chai').expect
 describe('Play Soldier Command', function () {
   h.loggedInAs('Sam', 'sam', function () {
     h.inGame(1, function () {
-      it('should work', function (done) {
+      it('should move the soldier', function (done) {
         var app = this.app
           , game = app.gameRoom.getGameModel(1);
 
@@ -47,11 +47,62 @@ describe('Play Soldier Command', function () {
             expect(sam.soldiers).to.equal(2);
             expect(sam.playedDevCard).to.equal(true);
             expect(sam.getNumResources()).to.equal(robbingPlayerResources + 1);
+            expect(sam.hasLargestArmy()).to.be.false;
             expect(brooke.getNumResources()).to.equal(robbedPlayerResources - 1);
 
             done();
           });
       });
+
+      it('should give sam the largest army', function (done) {
+        var app = this.app
+          , game = app.gameRoom.getGameModel(1);
+
+        var sam = game.players[0];
+        var brooke = game.players[1];
+        sam.soldiers = 2;
+        sam.playedDevCard = false;
+
+        expect(game.map.data.robber.x).to.equal(1);
+        expect(game.map.data.robber.y).to.equal(-1);
+        expect(sam.oldDevCards.soldier).to.equal(2);
+        expect(sam.soldiers).to.equal(2);
+        expect(sam.playedDevCard).to.be.false;
+        expect(sam.largestArmy).to.be.false;
+
+        var robbingPlayerResources = sam.getNumResources();
+        var robbedPlayerResources = brooke.getNumResources();
+
+        this.agent.post('/moves/Soldier')
+          .send({
+                 'playerIndex' : 0,
+                 'victimIndex' : 1,
+                 'robberSpot' : {
+                        'x' : -2,
+                        'y' : 2
+                  }
+          })
+          .expect(200)
+          .end(function (err, res) {
+
+            if (err) {
+              console.error(res.text);
+              return done(err);
+            }
+
+            expect(game.map.data.robber.x).to.equal(-2);
+            expect(game.map.data.robber.y).to.equal(2);
+            expect(sam.oldDevCards.soldier).to.equal(1);
+            expect(sam.soldiers).to.equal(3);
+            expect(sam.playedDevCard).to.be.true;
+            expect(sam.getNumResources()).to.equal(robbingPlayerResources + 1);
+            expect(brooke.getNumResources()).to.equal(robbedPlayerResources - 1);
+            expect(sam.hasLargestArmy()).to.be.true;
+
+            done();
+          });
+      });
+
     });
   });
 });
