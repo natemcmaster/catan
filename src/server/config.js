2e31859b7f021829a7541130ddde44102bb3d2b7
Nate McMaster <nate.mcmaster@gmail.com>
2014-04-02 12:31:44 -0600

    Add plugin pattern to config.js

--- src/server/config.js
+++ src/server/config.js
@@ -3,22 +3,29 @@ var logger = require('./resources').ConsoleLogger;
 var repo = require('./repo');
 
 function flatten(obj, d) {
-	d = d || {};
-	for (var x in obj) {
-		if (typeof obj[x] === 'object') {
-			flatten(obj[x], d);
-		} else if(typeof obj[x] === 'function'){
-			d[x] = obj[x];
-		}
-	}
-	return d;
+    d = d || {};
+    for (var x in obj) {
+        if (typeof obj[x] === 'object') {
+            flatten(obj[x], d);
+        } else if (typeof obj[x] === 'function') {
+            d[x] = obj[x];
+        }
+    }
+    return d;
 }
 
 var runtime = flatten(model);
-var memory = flatten(repo.mem);
+var repoPlugins = {
+    memory: flatten(repo.mem),
+    sqlite: flatten(repo.sqlite),
+    file: flatten(repo.file),
+};
 
-module.exports = function _setup_injection(inj){
-  inj.map(runtime);
-  inj.mapSingleton(memory); // store everything in memory
-  inj.singleton('Logger',logger);
+module.exports = function _setup_injection(inj, pl) {
+    pl = pl || 'memory';
+    if (!repoPlugins[pl])
+        throw new TypeError('Invalid persistence option: ' + pl);
+    inj.map(runtime);
+    inj.mapSingleton(repoPlugins[pl]);
+    inj.singleton('Logger', logger);
 }
