28cb376f1e041adac3c7d2fab844fd1cd53b7f2a
Nate McMaster <nate.mcmaster@gmail.com>
2014-04-11 12:37:52 -0600

    Put DAO inside game room

--- src/server/dao.js
+++ src/server/dao.js
@@ -13,35 +13,15 @@ module.exports = DAO;
  * @constructor
  */
 function DAO(deltaNumber, dataPath, $PersistanceLayer, $GameRoom, $GameModel) {
-  this.pl = $PersistanceLayer(dataPath);
-  this.gr = $GameRoom();
-  this.constructGame = $GameModel;
-  this.deltaNumber = deltaNumber;
-  this.currentDelta = 0;
+	this.pl = $PersistanceLayer(dataPath);
+	this.gr = $GameRoom();
+	this.constructGame = $GameModel;
+	this.deltaNumber = deltaNumber;
+	this.currentDelta = 0;
 
-  var data = JSON.parse(fs.readFileSync(path.join(__dirname,'/initial_data/_initialdata.json')));
+	var data = JSON.parse(fs.readFileSync(path.join(__dirname, '/initial_data/_initialdata.json')));
 	this.blank = data.blank;
 	this.constructGame = $GameModel;
-
-}
-
-/**
- * <pre>
- * Post-Condition: the created game room object has all of the previously
- * persisted data in it
- * </pre>
- * @method createGameRoom
- * @return {GameRoom} the created game room
- */
-DAO.prototype.createGameRoom = function (callback) {
-
-	this.pl.readAllUsers(function(error, users){
-		this.gr.users = users;
-		this.pl.getAllGameInfo(function(error, games){
-			this.gr.games = games;
-			callback(this.gr)
-		}.bind(this));
-	}.bind(this));
 
 }
 
@@ -53,28 +33,28 @@ DAO.prototype.createGameRoom = function (callback) {
  * @param {object} command
  * @return {int} command id
  */
-DAO.prototype.persistCommand = function (gameID, command) {
+DAO.prototype.persistCommand = function(gameID, command) {
 
-  this.pl.persistCommand(gameID, command, function(error){});
-  if(this.currentDelta >= this.deltaNumber){
+	this.pl.persistCommand(gameID, command, function(error) {});
+	if (this.currentDelta >= this.deltaNumber) {
 
-  	var gameData;
+		var gameData;
 
-  	for(i = 0; i < this.gr.games.length; i++){
-  		if(this.gr.games[i].id == gameID){
-  			gameData = this.gr.games[i];
-  			break;
-  		}
-  	}
+		for (i = 0; i < this.gr.games.length; i++) {
+			if (this.gr.games[i].id == gameID) {
+				gameData = this.gr.games[i];
+				break;
+			}
+		}
 
-  	this.pl.updateGame(gameID, gameData, function(error){
+		this.pl.updateGame(gameID, gameData, function(error) {
 
-  		this.currentDelta = 0;
+			this.currentDelta = 0;
 
-  	}.bind(this));
-  	
+		}.bind(this));
 
-  }
+
+	}
 
 
 
@@ -91,47 +71,48 @@ DAO.prototype.persistCommand = function (gameID, command) {
  * @param {fn} done
  * @return {int} the userid
  */
-DAO.prototype.createUser = function (user, password, done) {
-
-	this.pl.persistUser(user, password, function(error, userID){
-    if (error) return done(error)
-
-		this.gr.users[userID] = {'id' : userID,
-								 'username' : user,
-								 'password' : password};
-    done(null, this.gr.users[userID])
-
-	}.bind(this));
+DAO.prototype.createUser = function(user, password, done) {
+	this.pl.persistUser(user, password, function(error, userID) {
+		if (error) {
+			return done(error);
+		}
+		var u = {
+			'id': userID,
+			'username': user,
+			'password': password
+		};
+		done(null, u);
+	});
 }
 
-function randomTilify (game) {
-  var types = _.shuffle(['Desert', 'Wood', 'Wood', 'Wood', 'Wood', 'Ore', 'Ore', 'Ore', 'Sheep', 'Sheep', 'Sheep', 'Sheep', 'Wheat', 'Wheat', 'Wheat', 'Wheat', 'Brick', 'Brick', 'Brick']);
-  var newDesertLocation, prevDesertLocation;
-  game.map.hexGrid.hexes = _(game.map.hexGrid.hexes).map(function(s) {
-    return _(s).map(function(t) {
-      if (!t.isLand)
-        return t;
-      if (!t.landtype) {
-        prevDesertLocation = t.location;
-      }
-      var nexttype = types.pop();
-      t.landtype = (nexttype == 'Desert') ? null : nexttype;
-      if (!t.landtype) {
-        newDesertLocation = t.location;
-      }
-      return t;
-    }).value();
-  }).value();
+function randomTilify(game) {
+	var types = _.shuffle(['Desert', 'Wood', 'Wood', 'Wood', 'Wood', 'Ore', 'Ore', 'Ore', 'Sheep', 'Sheep', 'Sheep', 'Sheep', 'Wheat', 'Wheat', 'Wheat', 'Wheat', 'Brick', 'Brick', 'Brick']);
+	var newDesertLocation, prevDesertLocation;
+	game.map.hexGrid.hexes = _(game.map.hexGrid.hexes).map(function(s) {
+		return _(s).map(function(t) {
+			if (!t.isLand)
+				return t;
+			if (!t.landtype) {
+				prevDesertLocation = t.location;
+			}
+			var nexttype = types.pop();
+			t.landtype = (nexttype == 'Desert') ? null : nexttype;
+			if (!t.landtype) {
+				newDesertLocation = t.location;
+			}
+			return t;
+		}).value();
+	}).value();
 
-  // make sure the number is not on the desert tile
-  _(game.map.numbers).forIn(function(v,k,o) {
-    o[k]=_(v).map(function(t) {
-      if (t.x == newDesertLocation.x && t.y == newDesertLocation.y) {
-        return prevDesertLocation;
-      }
-      return t;
-    }).value();
-  });
+	// make sure the number is not on the desert tile
+	_(game.map.numbers).forIn(function(v, k, o) {
+		o[k] = _(v).map(function(t) {
+			if (t.x == newDesertLocation.x && t.y == newDesertLocation.y) {
+				return prevDesertLocation;
+			}
+			return t;
+		}).value();
+	});
 }
 
 /**
@@ -143,36 +124,36 @@ function randomTilify (game) {
  * @param {fn} done
  * @return {int} gameId
  */
-DAO.prototype.createGame = function (title, randomTiles, randomNumbers, randomPorts, done) {
+DAO.prototype.createGame = function(title, randomTiles, randomNumbers, randomPorts, done) {
 	var blank = _.cloneDeep(this.blank);
-	if(randomTiles){
-    randomTilify(blank)
+	if (randomTiles) {
+		randomTilify(blank)
 	}
-	if(randomPorts){
-		var types=_.shuffle(['Any','Any','Any','Any','Wood','Brick','Ore','Sheep','Wheat']);
-		_(blank.map.ports).forIn(function(v,k,o){
+	if (randomPorts) {
+		var types = _.shuffle(['Any', 'Any', 'Any', 'Any', 'Wood', 'Brick', 'Ore', 'Sheep', 'Wheat']);
+		_(blank.map.ports).forIn(function(v, k, o) {
 			var nexttype = types.shift();
-			if(nexttype == 'Any'){
+			if (nexttype == 'Any') {
 				o[k].inputResource = undefined;
 				o[k].ratio = 3;
-			} else{
+			} else {
 				o[k].inputResource = nexttype;
 				o[k].ratio = 2;
 			}
 		});
 	}
-	if(randomNumber){
-		var places = _.chain(blank.map.numbers).reduce(function(a,b){
+	if (randomNumber) {
+		var places = _.chain(blank.map.numbers).reduce(function(a, b) {
 			return a.concat(b);
-		},[])
-		.shuffle()
-		.value();
-		_(blank.map.numbers).forIn(function(v,k,o){
+		}, [])
+			.shuffle()
+			.value();
+		_(blank.map.numbers).forIn(function(v, k, o) {
 			var p = [places.shift()];
-			if(k!=2 && k!=12){
+			if (k != 2 && k != 12) {
 				p.push(places.shift());
 			}
-			o[k]=p;
+			o[k] = p;
 		});
 	}
 
@@ -183,11 +164,11 @@ DAO.prototype.createGame = function (title, randomTiles, randomNumbers, randomPo
 		model: model
 	};
 
-	this.pl.persistGame(game, function(error, gameID){
-    if (error) return done(error)
+	this.pl.persistGame(game, function(error, gameID) {
+		if (error) return done(error)
 		game.id = gameID;
 		this.gr.games[game.id] = game;
-    done(null, game)
+		done(null, game)
 	}.bind(this));
 }
 
@@ -200,7 +181,6 @@ DAO.prototype.createGame = function (title, randomTiles, randomNumbers, randomPo
  * @param {object} gameinfo
  * @return {void}
  */
-DAO.prototype.updateGame = function (id, gameinfo) {
-	this.pl.updateGame(id, gameinfo, function(error){});
+DAO.prototype.updateGame = function(id, gameinfo) {
+	this.pl.updateGame(id, gameinfo, function(error) {});
 }
-
